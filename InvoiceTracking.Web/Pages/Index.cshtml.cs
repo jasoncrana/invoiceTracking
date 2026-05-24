869b6d6269851927dd47459035c3253d8814e30b
Jason Rana <jasoncrana@gmail.com>
2026-05-23 21:05:16 -0500

    Index page created

--- InvoiceTracking.Web/Pages/Index.cshtml.cs
+++ InvoiceTracking.Web/Pages/Index.cshtml.cs
@@ -1,12 +1,37 @@
-using Microsoft.AspNetCore.Mvc;
+using InvoiceTracking.Core.Entities;
+using InvoiceTracking.Infrastructure.Data;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
 
 namespace InvoiceTracking.Web.Pages;
 
 public class IndexModel : PageModel
 {
-    public void OnGet()
-    {
+  private readonly ApplicationDbContext _context;
 
-    }
+  public IndexModel(ApplicationDbContext context)
+  {
+    _context = context;
+  }
+
+  public List<Invoice> Invoices { get; set; } = new();
+
+  public async Task OnGetAsync()
+  {
+    Invoices = await _context.Invoices
+             .Include(i => i.PaymentTicket)
+             .OrderByDescending(i => i.UploadedAt)
+             .ToListAsync();
+  }
+
+  // public string GetStatusBadge(PaymentStatus? status)
+  // {
+  //   return status switch
+  //   {
+  //     PaymentStatus.Paid => "bg-success",
+  //     PaymentStatus.PartiallyPaid => "bg-warning",
+  //     PaymentStatus.Overdue => "bg-danger",
+  //     _ => "bg-secondary" // default
+  //   };
+  // }
 }
