a633ddc293dd1baaf7ede0a8a3849b7a28051bf1
Jason Rana <jasoncrana@gmail.com>
2026-05-23 19:49:22 -0500

    DbContext created, SQLite packages installed

---
+++ InvoiceTracking.Infrastructure/Data/ApplicationDbContext.cs
@@ -0,0 +1,23 @@
+using InvoiceTracking.Core.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace InvoiceTracking.Infrastructure.Data;
+
+public class ApplicationDbContext : DbContext{
+  public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options){}
+
+  public DbSet<Invoice> Invoices {get; set;}
+  public DbSet<PaymentTicket> PaymentTickets {get; set;}
+
+  protected override void OnModelCreating(ModelBuilder modelBuilder){
+    base.OnModelCreating(modelBuilder);
+
+    // one-to-one relationship between Invoice and PaymentTicket.
+    // Confirmed with Finance Committee member responsible for making payments.
+    modelBuilder.Entity<Invoice>()
+                .HasOne(i => i.PaymentTicket)
+                .WithOne(t => t.Invoice)
+                .HasForeignKey<PaymentTicket>(t => t.InvoiceId)
+                .OnDelete(DeleteBehavior.Cascade);
+  }
+}
