46b27b1bba9776aa95d8243f4712a4006a28096f
Jason Rana <jasoncrana@gmail.com>
2026-05-23 20:06:37 -0500

    Web project configured

--- InvoiceTracking.Web/Program.cs
+++ InvoiceTracking.Web/Program.cs
@@ -1,26 +1,27 @@
+using InvoiceTracking.Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+// add services
 builder.Services.AddRazorPages();
+
+// register DbContext with SQLite
+builder.Services.AddDbContext<ApplicationDbContext>(options => 
+  options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
-{
+// configure the HTTP request pipeline
+if (!app.Environment.IsDevelopment()){
   app.UseExceptionHandler("/Error");
-  // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
   app.UseHsts();
 }
 
 app.UseHttpsRedirection();
-
+app.UseStaticFiles();
 app.UseRouting();
-
 app.UseAuthorization();
-
-app.MapStaticAssets();
-app.MapRazorPages()
-   .WithStaticAssets();
+app.MapRazorPages();
 
 app.Run();
